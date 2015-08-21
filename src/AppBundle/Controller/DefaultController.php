9affe46d2e6214c3611e1b3d1a566c84173eccd0
Grant Tepper <grant.tepper@gmail.com>
2015-08-21 03:22:18 -0400

    implements fos overrides

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render(':common:login.html.twig');
+        return $this->render('::base.html.twig');
     }
 }
