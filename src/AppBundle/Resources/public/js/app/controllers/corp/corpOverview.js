ddfb03d6481e5d0f071af77e85d8e1746790ca8a
Grant Tepper <grant.tepper@gmail.com>
2015-08-25 23:55:37 -0400

    updating show corp data

--- src/AppBundle/Resources/public/js/app/controllers/corp/corpOverview.js
+++ src/AppBundle/Resources/public/js/app/controllers/corp/corpOverview.js
@@ -4,7 +4,7 @@ angular.module('eveTool')
     .controller('corpOverviewController', ['$scope', function($scope, $http){
         $scope.selected_corp = null;
 
-        $scope.$on('selected_corporation', function(event, data){
+        $scope.$on('select_corporation', function(event, data){
             $scope.selected_corp = data;
         });
 
