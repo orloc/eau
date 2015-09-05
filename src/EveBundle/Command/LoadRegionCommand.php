352304997b0c1fbcc6855ec2154885a401277a7c
Grant Tepper <grant.tepper@gmail.com>
2015-09-05 05:21:42 -0400

    implementing load region command to pull down regions form eve crust

--- src/EveBundle/Command/LoadRegionCommand.php
+++ src/EveBundle/Command/LoadRegionCommand.php
@@ -3,6 +3,7 @@
 namespace EveBundle\Command;
 
 use EveBundle\Entity\Region;
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -22,11 +23,34 @@ class LoadRegionCommand extends ContainerAwareCommand
         $registry = $this->getContainer()->get('doctrine');
         $em = $registry->getManager('eve_data');
 
+        $client = new Client();
+
+        $result = $client->get('https://public-crest.eveonline.com/regions/');
+
+        if ($result->getStatusCode() === 200){
+
+            $json = $result->getBody()->getContents();
+
+            $data = $this->getContainer()->get('jms_serializer')->deserialize($json, 'array','json');
+
+            $regions = $data['items'];
+
+            foreach ($regions as $r){
+                $this->createRegion($r);
+            }
+
+
+
+
+        }
+
 
 
     }
 
     private function createRegion(array $data){
+
+        var_dump($data);die;
 
         $item = new Region();
 
--- src/EveBundle/Entity/Region.php
+++ src/EveBundle/Entity/Region.php
@@ -27,6 +27,90 @@ class Region
     /**
      * @ORM\Column(type="string")
      */
+    protected $region_url;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     protected $name;
 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set region_id
+     *
+     * @param integer $regionId
+     * @return Region
+     */
+    public function setRegionId($regionId)
+    {
+        $this->region_id = $regionId;
+
+        return $this;
+    }
+
+    /**
+     * Get region_id
+     *
+     * @return integer 
+     */
+    public function getRegionId()
+    {
+        return $this->region_id;
+    }
+
+    /**
+     * Set region_url
+     *
+     * @param string $regionUrl
+     * @return Region
+     */
+    public function setRegionUrl($regionUrl)
+    {
+        $this->region_url = $regionUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get region_url
+     *
+     * @return string 
+     */
+    public function getRegionUrl()
+    {
+        return $this->region_url;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Region
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
