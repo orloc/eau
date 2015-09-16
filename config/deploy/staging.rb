c866a8348b17450d6dda162edabfb249061881f4
Grant Tepper <grant.tepper@gmail.com>
2015-09-16 04:21:25 -0400

    updates statging and adds utf8 charset encoding

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,6 +5,8 @@ server '52.2.42.174', user: 'ubuntu', roles: %w{app web db}
 set :default_env, { path: "/home/ubuntu/bin:$PATH" }
 fetch(:default_env).merge!(symfony_env: fetch(:symfony_env))
 
+set :branch, 'staging'
+
 # Custom SSH Options
 # ==================
 # You may pass any option but keep in mind that net/ssh understands a
