014cd6c3bc893bd2472db0ce71d58e7fdb459e14
Grant Tepper <grant.tepper@gmail.com>
2015-10-19 00:27:48 -0400

    updating listener

--- src/AppBundle/EventListener/ActiveApiRequestListener.php
+++ src/AppBundle/EventListener/ActiveApiRequestListener.php
@@ -3,6 +3,7 @@
 namespace AppBundle\EventListener;
 
 use AppBundle\Entity\User;
+use Monolog\Logger;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,10 +20,13 @@ class ActiveApiRequestListener {
 
     protected $session;
 
-    public function __construct(TokenStorageInterface $storage, Router $router, Session $session) {
+    protected $log;
+
+    public function __construct(TokenStorageInterface $storage, Router $router, Session $session, Logger $logger) {
         $this->storage = $storage;
         $this->router = $router;
         $this->session = $session;
+        $this->log = $logger;
     }
 
     public function onRequest(GetResponseEvent $event){
@@ -31,21 +35,29 @@ class ActiveApiRequestListener {
         $request = $event->getRequest();
 
         $blackList = [
-            'api.server.status'
+            'api.server.status',
+            'template.serverstatus',
+            'template.slidebutton'
         ];
-
-        if ($request->attributes->get('_controller') === 'AppBundle\Controller\Admin\CharacterController::indexAction' || $this->session->has(self::ACTIVE_API_CHECK)){
-            $this->session->remove(self::ACTIVE_API_CHECK);
-            return;
-        }
 
         if (!$event->isMasterRequest()
             || $token === null
             || in_array($request->attributes->get('_route'), $blackList)
             || !strstr(explode('::',$request->attributes->get('_controller'))[0],'AppBundle')
         ){
+            $this->log->debug(sprintf("LISTENER Skipping %s", $request->attributes->get('_route')));
+
             return;
         }
+
+        if ($request->attributes->get('_controller') === 'AppBundle\Controller\Admin\CharacterController::indexAction'
+            || $this->session->has(self::ACTIVE_API_CHECK)
+        ){
+            $this->log->debug(sprintf("LISTENER Skipping %s", $request->attributes->get('_route')));
+            $this->session->remove(self::ACTIVE_API_CHECK);
+            return;
+        }
+
 
         if ($token->isAuthenticated()
             && ($user = $this->storage->getToken()->getUser()) instanceof User
@@ -53,6 +65,7 @@ class ActiveApiRequestListener {
             $characters =  $user->getCharacters();
 
             if ($characters->count() == 0){
+                $this->log->debug(sprintf("LISTENER REDIRECT for %s", $request->attributes->get('_route')));
                 $response = new RedirectResponse($this->router->generate('characters'));
                 $this->session->set(self::ACTIVE_API_CHECK, time());
                 $event->setResponse($response);
