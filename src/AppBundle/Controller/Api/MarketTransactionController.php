0ea25e72d36b4cd630e57060ad87be28b6bf6af0
Grant Tepper <grant.tepper@gmail.com>
2015-12-13 03:35:11 -0500

    reorganizes market transaciton controller to seperate parts

--- src/AppBundle/Controller/Api/MarketTransactionController.php
+++ src/AppBundle/Controller/Api/MarketTransactionController.php
@@ -30,57 +30,69 @@ class MarketTransactionController extends AbstractController implements ApiContr
         $date = $request->get('date', null);
         $type = $request->get('type', 'buy');
 
-
         if ($date === null){
             $arr = $this->getDoctrine()->getRepository('AppBundle:MarketTransaction')->findBy([
                 'account' => $account
             ]);
         } else {
-            $dt = Carbon::createFromTimestamp($date);
-
-            $repo = $this->getDoctrine()->getRepository('AppBundle:MarketTransaction');
-
-            if ($type === 'buy'){
-                $orders = $repo->getTotalBuyForDate($account, $dt);
-            } else if ($type === 'sell'){
-                $orders = $repo->getTotalSellForDate($account, $dt);
-            } else {
-                $orders = [];
-            }
-
-            $sorted = [];
-            foreach ($orders as $o){
-                if (!isset($sorted[$o->getItemName()])){
-                    $sorted[$o->getItemName()] = [];
-                }
-
-                if (!isset($sorted[$o->getItemName()][$o->getPrice()])){
-                    $sorted[$o->getItemName()][$o->getPrice()] = [];
-                }
-
-                $sorted[$o->getItemName()][$o->getPrice()][] = $o;
-            }
-
-            $reduced =  [];
-
-            foreach ($sorted as $name => $prices){
-                foreach ($prices as $price => $objs){
-                    $obj = array_reduce($objs, function($carry, $value){
-                        if ($carry === null){
-                            return $value;
-                        }
-
-                        return $value->setQuantity($carry->getQuantity()+$value->getQuantity());
-                    });
-
-                    $reduced[] = $obj;
-                }
-            }
+            $reduced = $this->reduceOrders(
+                $this->getOrderReference(
+                    $this->getOrders($account, $date, $type)
+                )
+            );
         }
 
         $json = $this->get('serializer')->serialize(isset($arr) ? $arr : $reduced, 'json');
 
         return $this->jsonResponse($json);
 
+    }
+
+    protected function reduceOrders(array $orders){
+        $reduced =  [];
+        foreach ($orders as $name => $prices){
+            foreach ($prices as $price => $objs){
+                $obj = array_reduce($objs, function($carry, $value){
+                    if ($carry === null){
+                        return $value;
+                    }
+                    return $value->setQuantity($carry->getQuantity()+$value->getQuantity());
+                });
+                $reduced[] = $obj;
+            }
+        }
+        return $reduced;
+    }
+
+    protected function getOrderReference(array $orders){
+        $sorted = [];
+        foreach ($orders as $o){
+            $itemName = $o->getItemName();
+            $itemPrice = $o->getPrice();
+            if (!isset($sorted[$itemName])){
+                $sorted[$itemName] = [];
+            }
+
+            if (!isset($sorted[$itemName][$itemPrice])){
+                $sorted[$itemName][$itemPrice] = [];
+            }
+            $sorted[$itemName][$itemPrice][] = $o;
+        }
+
+        return $sorted;
+    }
+
+    protected function getOrders(Account $account, $date, $type){
+        $dt = Carbon::createFromTimestamp($date);
+        $orders = [];
+        $repo = $this->getDoctrine()->getRepository('AppBundle:MarketTransaction');
+
+        if ($type === 'buy'){
+            $orders = $repo->getTotalBuyForDate($account, $dt);
+        } else if ($type === 'sell'){
+            $orders = $repo->getTotalSellForDate($account, $dt);
+        }
+
+        return $orders;
     }
 }
