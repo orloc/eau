eb59570a1193d734fc769dbc2d7c6fde1c314a7c
Grant Tepper <grant.tepper@gmail.com>
2016-01-17 05:03:47 -0500

    fixes bug with last sell in price helper and implements stubs for interface so composer passes

--- src/AppBundle/Service/DataManager/Corporation/BlueprintsManager.php
+++ src/AppBundle/Service/DataManager/Corporation/BlueprintsManager.php
@@ -2,15 +2,20 @@
 
 namespace AppBundle\Service\DataManager\Corporation;
 
-
-use AppBundle\Entity\Account;
-use AppBundle\Entity\AccountBalance;
-use AppBundle\Entity\Corporation;
-use Symfony\Component\OptionsResolver\Exception\OptionDefinitionException;
 use AppBundle\Service\DataManager\AbstractManager;
 use AppBundle\Service\DataManager\DataManagerInterface;
 use AppBundle\Service\DataManager\MappableDataManagerInterface;
 
-class AccountManager extends AbstractManager implements DataManagerInterface, MappableDataManagerInterface {
+class BlueprintsManager extends AbstractManager implements DataManagerInterface, MappableDataManagerInterface {
 
+
+    public function mapList($items, array $options){
+    }
+
+    public function mapItem($item){
+    }
+
+    public static function getName(){
+        return 'blueprint_manager';
+    }
 }
