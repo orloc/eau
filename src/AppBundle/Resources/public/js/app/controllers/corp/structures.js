884c119cd92762dfb8ea01b55a68dfb13d485f90
Grant Tepper <grant.tepper@gmail.com>
2016-07-04 06:25:09 -0700

    updating pos overview

--- src/AppBundle/Resources/public/js/app/controllers/corp/structures.js
+++ src/AppBundle/Resources/public/js/app/controllers/corp/structures.js
@@ -1,7 +1,53 @@
 'use strict';
 angular.module('eveTool')
     .controller('structureController', ['$scope', 'corporationDataManager', 'selectedCorpManager', function($scope, corporationDataManager, selectedCorpManager){
+        function getTimeToOffline(tower){
+            var date = moment(),
+                fuel = _.find(tower.fuel, function(f){
+                    return f.typeID !== "16275";
+                });
+
+            var perCycle = parseInt(tower.descriptors.fuel_consumption.quantity);
+            var secStatus = parseFloat(tower.descriptors.security) <= 0;
+            
+            if (secStatus){
+                perCycle = perCycle - (perCycle * 0.25) ;
+            }
+            var remaining = parseInt(fuel.quantity) / perCycle;
+            date.add(remaining, 'hours');
+            return parseFloat(date.format('x'));
+        }
+
+        function resolveTowerSize(tower, scale){
+            var size = _.find(tower.descriptors.attributes, function(d){
+                return d.attributeID === '1031';
+            }).valueInt;
+
+            if (typeof size !== 'undefined'){
+                switch(parseInt(size)){
+                    case 3:
+                        break;
+                    case 2:
+                        scale = scale  / 2;
+                        break;
+                    case 1:
+                        scale = scale / 4;
+                        break;
+                }
+            }
+
+            return scale;
+        }
+
+        
+        var tower_list = [];
         $scope.loading = true;
+        $scope.bases = [];
+        $scope.selected_corp = null;
+        $scope.image = {
+            width: 16
+        };
+        
         $scope.getTowerFuelQuantities = function(fuel, tower){
             if (typeof fuel !== 'undefined' && typeof tower !== 'undefined'){
                 var fuelVolume = parseInt(fuel.quantity) * parseFloat(fuel.type.volume);
@@ -25,42 +71,51 @@ angular.module('eveTool')
             return consumption * block.price;
         };
 
-        var getTimeToOffline = function(tower){
-            var date = moment(),
-                fuel = _.find(tower.fuel, function(f){
-                return f.typeID !== "16275";
-            });
-
-            var perCycle = parseInt(tower.descriptors.fuel_consumption.quantity);
-            var secStatus = parseFloat(tower.descriptors.security) <= 0;
-            if (secStatus){
-                perCycle = perCycle - (perCycle * 0.25) ;
-            }
-            var remaining = parseInt(fuel.quantity) / perCycle;
-            date.add(remaining, 'hours');
-            return parseFloat(date.format('x'));
-        };
-
-        $scope.selected_corp = null;
-        $scope.image = {
-            width: 16
-        };
-
         $scope.$watch(function(){ return selectedCorpManager.get(); }, function(val){
             if (typeof val.id === 'undefined'){
                 return;
             }
             $scope.selected_corp = val;
 
-            corporationDataManager.getStructures(val).then(function(data){
-                $scope.bases = data;
-                angular.forEach($scope.bases, function(b, k){
-                    $scope.bases[k].timeToOffline = getTimeToOffline(b);
+            corporationDataManager.getStructures(val)
+                .then(function(data) {
+                    return _.map(data, function(b){
+                        try {
+                            b.timeToOffline = getTimeToOffline(b);
+                        } catch (e){
+                            b.timeToOffline = 0;
+                            console.log(e);
+                        }
+                        return b;
+                    });
+                })
+                .then(function(data){
+                    tower_list = data;
+                    $scope.bases = _.filter(tower_list, function(t) {
+                        return t.state === 4;
+                    });
+                    $scope.loading = false;
                 });
-                $scope.loading = false;
-            });
         });
-
+        
+        $scope.showTowers = function(state){
+            switch (state){
+                case 'all':
+                    $scope.bases = tower_list;
+                    break;
+                case 'off':
+                    $scope.bases = _.filter(tower_list, function(t) {
+                        return t.state === 1;
+                    });
+                    break;
+                case 'on':
+                    $scope.bases = _.filter(tower_list, function(t) {
+                        return t.state === 4;
+                    });
+                    break;
+                default: break;
+            }
+        };
 
         $scope.hasAllianceAccess = function(settings){
             return  settings.allowAllianceMembers === '1';
@@ -83,27 +138,6 @@ angular.module('eveTool')
                 case 4:
                     return 'Online';
             }
-        }
-
-        function resolveTowerSize(tower, scale){
-            var size = _.find(tower.descriptors.attributes, function(d){
-                return d.attributeID === '1031';
-            }).valueInt;
-
-            if (typeof size !== 'undefined'){
-                switch(parseInt(size)){
-                    case 3:
-                        break;
-                    case 2:
-                        scale = scale  / 2;
-                        break;
-                    case 1:
-                        scale = scale / 4;
-                        break;
-                }
-            }
-
-            return scale;
-        }
+        };
 
     }]);
